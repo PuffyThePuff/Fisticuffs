4215d8165b1eb6f1b111849e60cb947a6cef3ea9
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-09-17 20:34:47 +0800

    Added scripts for bomb events

---
+++ Assets/Scripts/BombDisplay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BombDisplay : MonoBehaviour
+{
+    private Text text;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+        GameEvents.current.onSwipeUp += BombUsed;
+        GameEvents.current.onBuyBomb += BuyBomb;
+    }
+
+    private void BombUsed()
+    {
+        counter--;
+        text.text = counter.ToString();
+    }
+
+    private void BuyBomb()
+    {
+        counter++;
+        text.text = counter.ToString();
+    }
+}
